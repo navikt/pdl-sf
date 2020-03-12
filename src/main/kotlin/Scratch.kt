349590faca3bf7766d375e81f939e2d8a80931fe
torsteinnesby <ttnesby@gmail.com>
2020-03-12 15:17:18 +0100

    Playing with meta json in kotlin serialization - example

--- src/main/kotlin/Scratch.kt
+++ src/main/kotlin/Scratch.kt
@@ -1,12 +1,35 @@
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonPrimitive
+import mu.KotlinLogging
+import no.nav.pdlsf.json
+import java.io.File
 
+private val log = KotlinLogging.logger { }
+
+sealed class FnrBase
+object NoFnr : FnrBase()
+data class Fnr(val s: String) : FnrBase()
+
+fun JsonElement.getFnr(): FnrBase = runCatching {
+    val fnr = jsonObject.content["hentIdenter"]?.let { hi ->
+        hi.jsonObject.content["identer"]?.let { ids ->
+            ids.jsonArray.content.first {
+                it.jsonObject.content.containsValue(JsonPrimitive("FOLKEREGISTERIDENT")) &&
+                        it.jsonObject.content.containsValue(JsonPrimitive(false))
+            }
+                    .jsonObject
+                    .content["ident"]
+        }
+    }
+    fnr?.let { Fnr(fnr.toString()) } ?: NoFnr
+}
+        .onFailure { log.info { "Failure - ${it.localizedMessage}" }  }
+        .getOrDefault(NoFnr)
 
 fun main() {
 
-    val m = mutableMapOf<String, Int>(
-            "1" to 1,
-            "2" to 2,
-            "1" to 5
-    )
+    val person = json.parseJson(File("./src/test/resources/syntetiskPerson_1.json").readText())
 
-    println(m)
+    println(person.getFnr())
+
 }
